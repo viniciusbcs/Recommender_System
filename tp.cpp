db84cb70b58b13a35dbe47efe41b6cc1e3b06d30
Vinicius Braga Costa Dos Santos <viniciusbcs@viniciusbcs.local>
2018-04-14 11:56:51 -0300

    Arquivos base

---
+++ tp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//int a, b;
+	//char c;
+	//float d;
+	
+	//cin >> a >> b >> c >> d;
+
+		
+	return 0;
+}
