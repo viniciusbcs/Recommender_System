50ff24b3a0ececa0378e728c25b7c88c3251e9bc
VINÍCIUS BRAGA COSTA DOS SANTOS <viniciusbcs@VINICIUSs-iMac.local>
2018-04-14 13:56:43 -0300

    Acesso ao arquivo

--- tp.cpp
+++ tp.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	//int a, b;
-	//char c;
-	//float d;
-	
-	//cin >> a >> b >> c >> d;
+  FILE *fp;
+  FILE *fp2;
+  char ch, ch2;
 
-		
-	return 0;
+  if(argc!=3) {
+    cout << "Você esqueceu de digitar o nome do arquivo.\n";
+    exit(1);
+  }
+
+  if((fp=fopen(argv[1], "r"))==NULL) {
+    cout << "O arquivo não pode ser aberto.\n";
+    exit(1);
+  }
+	
+  if((fp2=fopen(argv[2],"r"))==NULL) {
+    cout << "O arquivo não pode ser aberto.\n";
+    exit(1);
+  } 
+
+  ch = getc(fp);
+  ch2 = getc(fp2);
+  
+  while (ch!=EOF) {
+    putchar(ch);
+    ch = getc(fp);
+  }
+
+  while (ch2!=EOF) {
+    putchar(ch2);
+    ch2 = getc(fp2);
+  }
+    
+  fclose(fp2);
+  fclose(fp);
+  return 0;
 }
