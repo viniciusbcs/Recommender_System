c497510fbe2c6a3f0d26e20b6304f248c32de791
Vinicius Braga Costa Dos Santos <viniciusbcs@gmail.com>
2018-04-16 19:46:59 -0300

    Entrada correta.

--- tp.cpp
+++ tp.cpp
@@ -13,7 +13,15 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
   
-  list<string> uid;
+  //list structur for ratings.csve
+  list<string> userlist;
+  list<string> productlist;
+  list<string> ratelist;
+  list<string> timelist;
+
+  //list structure for targets.csv
+  list<string> userlist2;
+  list<string> productlist2;
 
   //Opening ratings.csv and targets.csv
   char ch;
@@ -21,8 +29,10 @@ int main(int argc, char *argv[]) {
     cout << "Usage: tp ratings.csv targets.csv\n";
     return 1;
   }
-  ifstream in(argv[1]); // input
+
+  ifstream in(argv[1]); // input 1
   ifstream in2(argv[2]); // input 2
+  
   if(!in) {
     cout << "Cannot open ratings.csv.\n";
     return 1;
@@ -33,36 +43,52 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  char str[255];
-  char str2[255];
+  //ratings.csv read
+  char user[255];
+  char product[255];
+  char rate[255];
+  char time[255];
+
+  //targets.csv read
+  char user2[255];
+  char product2[255];
   
   //Ignoring the fst line
   in.ignore(100, '\n');
-  in2.ignore(20, '\n');
+  in2.ignore(100, '\n');
 
   //reading ratings.csv
   while(in) { 
-    in.getline(str, 255, ':');  // delim defaults to '\n'    
-    //in.ignore(1,':');
-    uid.push_back(str);
-    //if(in) cout << str << endl;
+    in.getline(user, 255, ':');  // delim defaults to '\n'    
+    in.getline(product, 255, ',');
+    in.getline(rate, 255, ',');
+    in.getline(time, 255, '\n');
+    userlist.push_back(user);
+    productlist.push_back(product);
+    ratelist.push_back(rate);
+  }
+  
+  while(in2) {
+    in2.getline(user2, 255, ':');
+    in2.getline(product2, 255, '\n');
+    userlist2.push_back(user2);
+    productlist2.push_back(product2);
   }
 
-  while(!uid.empty()){
-    cout << uid.front();
-    uid.pop_front();
+  /* Out teste
+  while(!userlist.empty()){
+    cout << userlist.front();
+    userlist.pop_front();
     cout << "\n";
   }
-
-  cout << "TESTE " << uid.front() << "\n";
   
-  //reading targests.csv
-  while(in2){
-    in2.getline(str2, 255);
-    
-    //if(in2) cout << str2 << endl;
+  while(!productlist2.empty()){
+    cout << productlist2.front();
+    productlist2.pop_front();
+    cout << "\n";
   }
-  
+  */
+
   in.close();
   in2.close();
   return 0;
